fd38243dfef93ff5be8e95d0fadad2668474af14
kylim99 <zxzx05050@naver.com>
2023-11-11 18:31:11 +0900

    feat: 초기 매시지 출력 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+public class OutputView {
+    private final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public void printWelcomeMessage(){
+        System.out.println(WELCOME_MESSAGE);
+    }
+}
