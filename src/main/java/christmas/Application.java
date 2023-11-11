0f40b9cd941398af1e18044e2df1ca2e90dd8a40
kylim99 <zxzx05050@naver.com>
2023-11-11 18:11:34 +0900

    feat: 매인 함수 구현

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.controller.ChristmasApplication;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ChristmasApplication christmasApplication = new ChristmasApplication();
+        christmasApplication.run();
     }
 }
---
+++ src/main/java/christmas/controller/ChristmasApplication.java
@@ -0,0 +1,7 @@
+package christmas.controller;
+
+public class ChristmasApplication {
+    public void run(){
+
+    }
+}
