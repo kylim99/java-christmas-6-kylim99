35c8018f138daf5245b7238ec2326d53254b68d1
kylim99 <zxzx05050@naver.com>
2023-11-13 05:22:43 +0900

    feat: 주말 이벤트 적용 기능 구현

--- src/main/java/christmas/model/Benefits.java
+++ src/main/java/christmas/model/Benefits.java
@@ -1,12 +1,32 @@
 package christmas.model;
 
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Benefits {
 
     private int benefitPrice;
     public Benefits(ConfirmOrder confirmOrder){
         applyDayBenefit(confirmOrder);
+        applyWeekendBenefit(confirmOrder);
+    }
+
+    private void applyWeekendBenefit(ConfirmOrder confirmOrder) {
+        if(isWeekend(confirmOrder.getWeek())){
+            addBenefitPricePerMain(confirmOrder.getConfirmOrder());
+        }
+    }
+
+    private void addBenefitPricePerMain(Map<Food, Integer> confirmOrder) {
+        for(Entry<Food, Integer> entry : confirmOrder.entrySet()){
+            if(entry.getKey().getType() == Type.MAIN){
+                benefitPrice += 2023 * entry.getValue();
+            }
+        }
+    }
+
+    private boolean isWeekend(Week week) {
+        return week.equals(Week.FRIDAY) || week.equals(Week.SATURDAY);
     }
 
     private void applyDayBenefit(ConfirmOrder confirmOrder) {
