2047f6509cfbce4ae5af94124950d13789e7568d
kylim99 <zxzx05050@naver.com>
2023-11-13 02:41:25 +0900

    feat: 주문사항 입력 기능 구현

---
+++ src/main/java/christmas/model/Order.java
@@ -0,0 +1,25 @@
+package christmas.model;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Order {
+    private Map<String,Integer> orderSheet = new HashMap<>();
+
+    public Order(String input){
+        List<String> order = Arrays.stream(input.split(",")).toList();
+        putOrder(order);
+    }
+
+    private void putOrder(List<String> orders) {
+        this.orderSheet = orders.stream()
+                .map(order -> order.split("-"))
+                .collect(Collectors.toMap(
+                        splitOrder -> splitOrder[0],
+                        splitOrder -> Integer.parseInt(splitOrder[1])
+                ));
+    }
+}
--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,15 +1,23 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
 
 public class InputView {
 
     private final String DATE_INPUT_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private final String MENU_INPUT_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public int readDate() {
         System.out.println(DATE_INPUT_MESSAGE);
         String input = Console.readLine();
         return Integer.parseInt(input);
     }
+
+    public String readMenu(){
+        System.out.println(MENU_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
 
 }
