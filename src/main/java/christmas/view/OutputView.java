40cfa1b2c66c9ee89b6173544a2b4dca0da05c39
kylim99 <zxzx05050@naver.com>
2023-11-13 05:45:24 +0900

    feat: 해택 내역 출력 시작 메시지 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -10,6 +10,7 @@ public class OutputView {
     DecimalFormat decFormat = new DecimalFormat("###,###");
     private final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private final String INIT_ORDER_MENU_MESSAGE = "<주문 메뉴>";
+    private final String INIT_BENEFIT_MESSAGE = "<해택 내역>";
     private final String INIT_TOTAL_PRICE = "<할인 전 총주문 금액>";
     private final String ORDER_MENU_MESSAGE = "%s %d개";
 
@@ -32,5 +33,9 @@ public class OutputView {
     private void printTotalPriceBeforeEvent(AtomicInteger price) {
         System.out.println(INIT_TOTAL_PRICE);
         System.out.println(decFormat.format(price)+"원");
+    }
+
+    public void printBenefitTitle() {
+        System.out.println(INIT_BENEFIT_MESSAGE);
     }
 }
