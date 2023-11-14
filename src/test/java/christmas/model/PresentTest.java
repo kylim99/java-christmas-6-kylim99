425a9daa12b0a3a7851b0dc2c588034bdf33f52c
kylim99 <zxzx05050@naver.com>
2023-11-15 03:35:57 +0900

    test: PresentTest 작성

---
+++ src/test/java/christmas/model/PresentTest.java
@@ -0,0 +1,29 @@
+package christmas.model;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.HashMap;
+import org.junit.jupiter.api.Test;
+
+public class PresentTest {
+
+
+
+    @Test
+    void 증정품_테스트(){
+        HashMap<Food,Integer> map = new HashMap<>();
+        Food food1 = new Food("티본스테이크",55000,Type.MAIN);
+        Food food2 = new Food("양송이수프",6000,Type.APPETIZER);
+        Food food3 = new Food("레드와인",60000,Type.BEVERAGE);
+        map.put(food1,2);
+        map.put(food2,2);
+        map.put(food3,1);
+        ConfirmOrder confirmOrder = new ConfirmOrder(map,3,Week.SUNDAY);
+
+        Present present = new Present(confirmOrder);
+
+        assertThat(present.getPresent()).isEqualTo("샴페인 1개");
+        assertThat(present.getPresentLog()).isEqualTo("증정 이벤트: -25,000원");
+        assertThat(present.getPresentPrice()).isEqualTo(25000);
+    }
+}
